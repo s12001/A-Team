4ca2d1c8cad52a2c3f5c4fd8768905e3554cbd80
s13003 <s13003@std.it-college.ac.jp>
2014-06-12 16:29:32 +0900

    ログイン処理追加

--- StudentManagementSystem/src/jp/ac/it_college/std/sms/java/LoginWindowController.java
+++ StudentManagementSystem/src/jp/ac/it_college/std/sms/java/LoginWindowController.java
@@ -1,4 +1,9 @@
 package jp.ac.it_college.std.sms.java;
 
+
 public class LoginWindowController extends BaseController {
+
+    public void Login() {
+        mMain.changePrimaryStageScene("学生一覧", "StudentListWindow");
+    }
 }
