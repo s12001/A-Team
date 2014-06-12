f1bdd8867245dce0cde52380bb46cd418993ecab
Yuki Gima <sp.lemontea@gmail.com>
2014-06-12 16:00:13 +0900

    ログイン画面表示

--- StudentManagementSystem/src/jp/ac/it_college/std/sms/java/Main.java
+++ StudentManagementSystem/src/jp/ac/it_college/std/sms/java/Main.java
@@ -1,9 +1,15 @@
 package jp.ac.it_college.std.sms.java;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+    private Stage mPrimaryStage;
 
     public static void main(String[] args) {
         launch(args);
@@ -11,6 +17,23 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        mPrimaryStage = primaryStage;
+        mPrimaryStage.setResizable(false);
+        mPrimaryStage.resizableProperty().setValue(false);
+        changePrimaryStageScene("Login", "LoginWindow");
+    }
 
+    public void changePrimaryStageScene(String title, String fxml) {
+        try {
+            mPrimaryStage.setTitle(title);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/" + fxml + ".fxml"));
+            Pane pane = loader.load();
+            BaseController controller = loader.getController();
+            controller.setMain(this);
+            mPrimaryStage.setScene(new Scene(pane));
+            mPrimaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
