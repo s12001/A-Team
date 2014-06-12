99826d3f4056426ff812d3d170789a07091b49ef
Yuki Gima <sp.lemontea@gmail.com>
2014-06-12 21:06:38 +0900

    DataBaseConnectionクラス作成

---
+++ StudentManagementSystem/src/jp/ac/it_college/std/sms/java/DataBaseConnection.java
@@ -0,0 +1,49 @@
+package jp.ac.it_college.std.sms.java;
+
+import java.sql.*;
+
+public class DataBaseConnection {
+    private static DataBaseConnection instance;
+    private Connection mConnection;
+
+    public static DataBaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DataBaseConnection();
+        }
+        return instance;
+    }
+
+    public DataBaseConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            mConnection = DriverManager.getConnection(
+                    "jdbc:mysql://172.16.40.3/pbl", "pbl", "itcollege"
+            );
+        } catch (ClassNotFoundException | InstantiationException
+                | IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Statement getStatement() {
+        Statement statement = null;
+        try {
+            statement = mConnection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return statement;
+    }
+
+    public PreparedStatement getPreparedStatement(String sql) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = mConnection.prepareStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return preparedStatement;
+    }
+}
