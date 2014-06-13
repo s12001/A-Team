7d9ebfd9718fa6e28ef6f3c8375bcdde593b4162
Yuki Gima <sp.lemontea@gmail.com>
2014-06-13 14:22:01 +0900

    不要なインポート削除

--- StudentManagementSystem/src/jp/ac/it_college/std/sms/java/Main.java
+++ StudentManagementSystem/src/jp/ac/it_college/std/sms/java/Main.java
@@ -7,7 +7,6 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.ResultSet;
 
 public class Main extends Application {
     private Stage mPrimaryStage;
