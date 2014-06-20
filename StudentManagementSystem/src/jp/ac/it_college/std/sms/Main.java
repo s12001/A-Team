2a7a24279ea2c17d6cefceab52b88b223115363d
s12001 <s12001@std.it-college.ac.jp>
2014-06-20 17:54:35 +0900

    コントローラーをcontrollerパッケージにまとめた

--- StudentManagementSystem/src/jp/ac/it_college/std/sms/Main.java
+++ StudentManagementSystem/src/jp/ac/it_college/std/sms/Main.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import jp.ac.it_college.std.sms.controller.BaseController;
+import jp.ac.it_college.std.sms.controller.LoginWindowController;
 
 import java.io.IOException;
 
